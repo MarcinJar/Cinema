67e5fd585f30d689dc38037065bc82001192da7e
MarcinJar <m.jarmulowski@wp.pl>
2017-06-17 21:22:57 +0200

    Add movie logic

---
+++ CinemaReservation/CinemaReservation.Core/DataLogic/IDataLogic/IMovieLogic.cs
@@ -0,0 +1,22 @@
+﻿using CinemaReservation.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaReservation.Core.DataLogic.IDataLogic
+{
+    public interface IMovieLogic
+    {
+        Movie Get(int movieDBKey);
+
+        IEnumerable<Movie> GetAll();
+
+        Movie Add(Movie movieToCreate);
+
+        Movie Update(Movie movieToUpdate);
+
+        bool Delete(int movieDBKey);
+    }
+}
---
+++ CinemaReservation/CinemaReservation.Core/DataLogic/MovieLogic.cs
@@ -0,0 +1,46 @@
+﻿using CinemaReservation.Core.DataLogic.IDataLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaReservation.Model.Models;
+using CinemaReservation.InterfaceDataAccess;
+
+namespace CinemaReservation.Core.DataLogic
+{
+    public class MovieLogic : IMovieLogic
+    {
+        private IMovieRepository movieRepository;
+
+        public MovieLogic(IMovieRepository movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+
+        public Movie Add(Movie movieToCreate)
+        {
+            return this.movieRepository.Add(movieToCreate);
+        }
+
+        public bool Delete(int movieDBKey)
+        {
+            return this.movieRepository.Delete(movieDBKey);
+        }
+
+        public Movie Get(int movieDBKey)
+        {
+            return this.movieRepository.Get(movieDBKey);
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return this.movieRepository.GetAll();
+        }
+
+        public Movie Update(Movie movieToUpdate)
+        {
+            return this.movieRepository.Update(movieToUpdate);
+        }
+    }
+}
