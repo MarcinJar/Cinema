a2cad904783b80e99b6821f197f5cd83d8d1abbf
Marcin Jarmułowski <m.jarmulowski@wp.pl>
2017-06-20 22:11:03 +0200

    Add logic for the reservation

---
+++ CinemaReservation/CinemaReservation.Core/DataLogic/ReservationLogic.cs
@@ -0,0 +1,46 @@
+﻿using CinemaReservation.Core.DataLogic.IDataLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaReservation.Model.Models;
+using CinemaReservation.InterfaceDataAccess;
+
+namespace CinemaReservation.Core.DataLogic
+{
+    class ReservationLogic : IReservationLogic
+    {
+        private IReservationRepository reservationRepository;
+
+        public ReservationLogic(IReservationRepository reservationRepository)
+        {
+            this.reservationRepository = reservationRepository;
+        }
+
+        public Reservation Add(Reservation toCreate)
+        {
+            return this.reservationRepository.Add(toCreate);
+        }
+
+        public bool Delete(int DBKey)
+        {
+            return this.reservationRepository.Delete(DBKey);
+        }
+
+        public Reservation Get(int DBKey)
+        {
+            return this.reservationRepository.Get(DBKey);
+        }
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return this.reservationRepository.GetAll();
+        }
+
+        public Reservation Update(Reservation toUpdate)
+        {
+            return this.reservationRepository.Update(toUpdate);
+        }
+    }
+}
